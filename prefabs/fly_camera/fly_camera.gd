0a13373f0b17bc564c19f23e97f00ab8cf6b38a0
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2026-08-28 23:18:49 +0700

    Fix broken flycam
    NOTE: I can see this was made in one monolithic commit. Don't do
    monoliths, kids. And always check if you broke something.

--- prefabs/fly_camera/fly_camera.gd
+++ prefabs/fly_camera/fly_camera.gd
@@ -1,38 +1,43 @@
 extends Node3D
 
+@export var speed: float = 100
+@export var sensitivity: float = 1
 
-@onready var x_coordinate = $camera_position_x
-@onready var y_coordinate = $camera_position_y
-@onready var z_coordinate = $camera_position_z
-
-var sensitivity = 2.0
-
-func _process(delta):
-	x_coordinate.text = str(self.position.x)
-	y_coordinate.text = str(self.position.y)
-	z_coordinate.text = str(self.position.z)
-	if Input.is_action_pressed("flycam_up"):
-		position.y = position.y + 1
-	if Input.is_action_pressed("flycam_down"):
-		position.y = position.y - 1
-	if Input.is_action_pressed("flycam_left"):
-		position.x = position.x + 1
-	if Input.is_action_pressed("flycam_right"):
-		position.x = position.x - 1
-	if Input.is_action_pressed("flycam_forward"):
-		position.x = position.x + 1
-		position.y = position.y + 1
-		position.z = position.z + 1
-	if Input.is_action_pressed("flycam_backward"):
-		position.x = position.x - 1
-		position.y = position.y - 1
-		position.z = position.z - 1
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func _input(event):
+
+func _physics_process(delta: float) -> void:
+	var movement := Vector3.ZERO
+
+	if Input.is_action_pressed("flycam_forward"):
+		movement -= global_basis.z
+	if Input.is_action_pressed("flycam_backward"):
+		movement += global_basis.z
+	if Input.is_action_pressed("flycam_left"):
+		movement -= global_basis.x
+	if Input.is_action_pressed("flycam_right"):
+		movement += global_basis.x
+	if Input.is_action_pressed("flycam_up"):
+		movement += global_basis.y
+	if Input.is_action_pressed("flycam_down"):
+		movement -= global_basis.y
+
+	if movement == Vector3.ZERO:
+		return
+
+	position += movement.normalized() * delta * (
+		speed if !Input.is_key_pressed(KEY_SHIFT) else speed * 2
+	)
+
+
+func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(-event.relative.x * sensitivity))
-		rotate_x(deg_to_rad(event.relative.y * sensitivity))
-		rotation.z = 0
+		rotation.y += deg_to_rad(-event.relative.x * sensitivity)
+		rotation.x += deg_to_rad(-event.relative.y * sensitivity)
+	elif event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			speed *= 1.25
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			speed /= 1.25
